e8b3b34595c304936d96eff6ad2417201cbea21c
Nathan Bateson <nathanjohnbateson@gmail.com>
2024-07-14 11:13:08 +0100

    Created frontend README.md Created a plan for how data should be structured

--- backend/src/main/java/dev/nahtan/Main.java
+++ backend/src/main/java/dev/nahtan/Main.java
@@ -5,6 +5,25 @@ import org.tinylog.Logger;
 public class Main {
 
     public static void main(String... args) {
-        Logger.info("Hello World");
+        Logger.info("Starting Backend Server");
+
+        /*
+            DATA LAYOUT
+            - Users each need an email, username, password and an internal ID
+            - Each internal ID is unique and can be used to find the shifts of the user
+            - Shifts each need a shift id, user id, start date, end date, duration, ?rate, location name, payment received
+                (rate can be nullable as some jobs may pay by the shift rather than by hour)
+                (although duration could be calculated from the start and end date, it's simpler to just store it pre-calculated for easier use later)
+
+            OTHER USER DATA
+            - Total Earned
+            - Additional Incomes e.g. Tips that are calculated separately
+            - List of stored locations
+            - Total Time Worked
+            - Avg. hourly rate including other incomes
+         */
+
+
+
     }
 }
---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+# ShiftClocker Frontend
+
+The frontend will be worked on later, for now the backend is being produced.
